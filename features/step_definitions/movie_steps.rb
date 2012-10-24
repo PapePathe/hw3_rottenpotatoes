cfe471c1fba7aac2982531af400cbb86674974cb
Papa Pathe SENE <pathe.sene@gmail.com>
2012-10-24 05:05:29 +0000

    completed hw3 part1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,12 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create! do |m|
+      m.title        = movie[:title]
+      m.rating       = movie[:rating]
+      m.release_date = movie[:title]
+    end
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
